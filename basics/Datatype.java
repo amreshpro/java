a8e72c10918e23431d70a68173089f93dd1b3c12
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-15 22:56:22 +0530

    java steps

---
+++ basics/Datatype.java
@@ -0,0 +1,15 @@
+class Datatyoe{
+  public static void main(String[] args) {
+    
+    // questions 1
+    byte a =5;
+    byte b =10;
+    byte c =a+b;  // error cannot conver from int to byte
+    System.out.println(c);
+
+    // questions 2
+    char a1 = 'A';
+    int b1 = a;
+    System.out.println(b1);
+  }
+}
