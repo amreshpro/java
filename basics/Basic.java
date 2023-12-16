127ef457ebea8b99bc865c778d2d3a5931c7e532
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-16 21:21:55 +0530

    add keyword and some important note

---
+++ basics/Basic.java
@@ -0,0 +1,8 @@
+class Basic{
+  public static void main(String[] args) {
+    byte a = 5;
+    byte b = 10;
+    int c = a+b;
+    System.out.println(c);
+  }
+}
--- basics/Datatype.java
+++ basics/Datatype.java
@@ -1,11 +1,14 @@
-class Datatyoe{
+class Datatype{
   public static void main(String[] args) {
     
     // questions 1
     byte a =5;
     byte b =10;
-    byte c =a+b;  // error cannot conver from int to byte
-    System.out.println(c);
+    // byte c =a+b;  // error cannot conver from int to byte
+    float d = 15.6f;
+    float e = 15.6f;
+float f = d+e;
+    System.out.println(f);
 
     // questions 2
     char a1 = 'A';
