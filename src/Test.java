0cc70e37cdc22dd5ca53eb30aaec49cff38eb062
Amresh Maurya <amresh.terminal@gmail.com>
2024-08-11 23:40:23 +0530

    test

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ package src;
 
 class Test{
     public static void main(String[] args) {
-     System.out.print("Hello Test");   
+     System.out.print("Hello Test!");   
      System.console();
     }
 }
