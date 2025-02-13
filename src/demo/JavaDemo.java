441ec44a570485ef00c8999e89bdb154ac267e28
Amresh Maurya <amresh.terminal@gmail.com>
2025-02-13 22:49:57 +0530

    format

--- src/demo/JavaDemo.java
+++ src/demo/JavaDemo.java
@@ -2,7 +2,7 @@ package src.demo;
 
 import java.util.Scanner;public class JavaDemo {
   public static void main(String[] args){
-      int n ;
+        int n ;
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
   }
